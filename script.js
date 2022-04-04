8b6af91af30409f8e16525b0f98178473d1ede20
Mark Gerken <mgerken86@gmail.com>
2022-04-04 11:25:02 -0600

    link js and css

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 prompt("Choose rock, paper, or scissors");
+
+const choices = [rock, paper, scissors];
+
+const randomChoice = Math.floor(Math.random() * choices.length);
+const computerChoice = choices[randomChoice];
+
+console.log("computerChoice");
